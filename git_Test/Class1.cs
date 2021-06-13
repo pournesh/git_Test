4c01458ec9dd4e9dada434be7a9036382ef44b51
pournesh <pournesh96@gmail.com>
2021-06-13 15:05:32 +0530

    Hello world sample code

---
+++ git_Test/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace git_Test
+{
+    class Class1
+    {
+        public int x = 5;
+        
+    }
+}
---
+++ git_Test/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace git_Test
+{
+    class Class2 : Class1
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            Console.WriteLine(c.x.ToString() + "Hello World!");
+        }
+    }
+}
